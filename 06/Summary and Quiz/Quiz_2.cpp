fb2987d1608eb93b2bd3f923b423be0d4692376a
Sparking2 <sparking02@yahoo.com.mx>
2021-03-12 13:25:19 -0600

    2021-03-12

---
+++ 06/Summary and Quiz/Quiz_2.cpp
@@ -0,0 +1,20 @@
+//Write a file named constants.h that makes the following program run.
+//If your compiler is C++17 capable, use inline constexpr variables.
+//Otherwise, use normal constexpr variables.
+//You can pick any value you like for max_class_size.
+
+#include "constants.h"
+#include <iostream>
+
+int Quiz_2() {
+	std::cout << "How many students are in your class? ";
+	int students{};
+	std::cin >> students;
+
+	if (students > constants::max_class_size)
+		std::cout << "There are too many students in this class";
+	else
+		std::cout << "This class isn't too large";
+
+	return 0;
+}
